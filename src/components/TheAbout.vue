9e716e5b83f4526554928706bddeb5d545154373
CyndelHerolt <herolt.cyndel@gmail.com>
2023-09-19 11:40:21 +0200

    feat: titles

---
+++ src/components/TheAbout.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+const awesome = ref(true);
+const items = [{ message: 'Foo' }, { message: 'Bar' }]
+</script>
+
+<template>
+
+  <h2>{{ msg }}</h2>
+
+  <hr>
+
+  <h3>Tests</h3>
+
+  <button @click="awesome = !awesome">Basculer</button>
+  <ul>
+    <li v-if="awesome ">super</li>
+    <li v-else>pas super</li>
+  </ul>
+
+  <hr>
+
+  <h3>Boucles</h3>
+
+  <ul>
+    <li v-for="item in items">
+      {{ item.message }}
+    </li>
+  </ul>
+</template>
